d2f9c6765655624782a65824fc080c4246fe77b6
Gabriel <gabrielmlt@protonmail.ch>
2021-10-25 16:59:02 +0200

    Add fonts & theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      primary: "#29C5A8",
+      primaryLight: "#88E7D5",
+      primaryDark: "#135D50",
+      secondary: "#595A67",
+      secondaryLight: "#9A9BA7",
+      secondaryDark: "#1F1F23"
+    },
+    fontFamily: {
+      normal: ["Lato-Regular"],
+      bold: ["Lato-Bold"],
+      black: ["Lato-Black"],
+      thin: ["Lato-Thin"],
+      italic: ["Lato-Italic"],
+      medium: ["Lato-Medium"],
+    }
   },
   variants: {
     extend: {},
