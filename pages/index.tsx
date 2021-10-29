3954b60b86a1d383772f033026094b4fa6f80503
Gabriel <gabrielmlt@protonmail.ch>
2021-10-29 14:34:27 +0200

    Add transition

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
 	return (
 		<section className="flex justify-center">
-			<div className="rounded-lg shadow-xl bg-primary my-96 py-6 px-4">
+			<div className="rounded-lg shadow-xl bg-primary my-96 py-6 px-4 transition-shadow ease-linear duration-200 hover:shadow-none">
 				<a className="font-normal py-4 px-6" href="https://www.github.com/Ikonera/boilerplate-nextjs" target="_blank">
 					Ikonera - Nextjs boilerplate repository
 				</a>
