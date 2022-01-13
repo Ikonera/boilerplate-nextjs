d3d085f3df6cd7121997c250d63eecc776b90b4c
Ikonera <gabrielmlt@protonmail.ch>
2022-01-13 20:59:18 +0100

    style: better virgin homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,20 @@ import {
 const Home: NextPage = () => {
 	return (
 		<section>
-			<Grid container justifyContent="center">
-				<Button href="https://github.com/Ikonera/boilerplate-nestjs" target="_blank">
-					Ikonera - Nextjs boilerplate repository
-				</Button>
+			<Grid container direction="column" alignContent="center" marginTop="15%" alignItems="center">
+				<Grid item>
+					<Typography>
+						Welcome to this NextJS / TS virgin boilerplate !
+					</Typography>
+					<Typography>
+						Feel free to made her what you want :D
+					</Typography>
+				</Grid>
+				<Grid item marginTop="2%">
+					<Button href="https://github.com/Ikonera/boilerplate-nestjs" target="_blank">
+						Ikonera - Nextjs boilerplate repository
+					</Button>
+				</Grid>
 			</Grid>
 		</section>
 	);
